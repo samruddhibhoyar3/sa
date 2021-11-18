61332b94aaf9af7d0b0c164bb1ead08c3c84347e
Samruddhi Bhoyar <52337446+samruddhibhoyar3@users.noreply.github.com>
2021-11-18 11:27:25 +0530

    Update 9.next permutation.cpp

--- 9.next permutation.cpp
+++ 9.next permutation.cpp
@@ -1,1 +1,32 @@
 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) 
+    {
+        int n = nums.size(), k, l;
+      
+      //finding the breakpoint, that is number just smaller after that breakpoint occurs
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
+        }
+      
+      // if 54321 is given then simply reverse
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} 
+      else 
+      {
+    	    for (l = n - 1; l > k; l--)
+          {
+                if (nums[l] > nums[k])
+                {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
+    }
+};
